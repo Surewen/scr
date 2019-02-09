6590eac18a3bfdf15854f02b7f4b1ec8877d9443
Surewen <github.com>
2019-02-09 11:26:19 -0800

    added header files, multi process implementation of atm

---
+++ atm.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+#include <atm_communication.h>
+#include <atm_service.h>
+
+pid_t communications_pid = fork();
+
+int main(int argc, char const *argv[])
+{
+	if(communications_pid == 0){
+		//Communication process
+		atm_communication();
+	}
+	else if (communications_pid > 0){
+		//Service process
+		atm_service();
+	}
+	else{
+		std::cout << "Fork fail!" << '\n';
+	}
+	return 0;
+}
