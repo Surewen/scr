02da977ece28fad9101b611508bc90f60a3f510d
Surewen <github.com>
2019-02-09 16:54:42 -0800

    added timed loop

--- atm_service.cpp
+++ atm_service.cpp
@@ -1,8 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <iostream>
+#include <ctime>
 
 bool service_running = true;
+
+
 
 int withdraw();
 int deposit();
@@ -13,9 +16,36 @@ int atm_service()
 {
 	init_service();
 
+	int current_time = 0;
+	int previous_time = 0;
+	int delta_time = 0;
+
 	while(service_running)
 	{
-
+		if(delta_time > 500)
+		{
+			previous_time = time(0);
+			//ALL LOGIC GOES HERE
+			draw_menu();
+			char key_pressed;
+			cin >> key_pressed;
+			switch(key_pressed)
+			{
+				case 1:
+				break;
+				case 2:
+				break;
+				case 3:
+				break;
+				case 4:
+				break;
+				break;
+				default:
+				break;
+			}
+		}
+		current_time = time(0);
+		delta_time = current_time - previous_time;
 	}
 }
 
@@ -32,6 +62,12 @@ int deposit()
 int draw_menu()
 {
 	system("clear");
+	std::cout << 
+	"Main menu:\n" <<
+	"1. Show balance\n" <<
+	"2. Withdraw cash\n" <<
+	"3. Deposit cash\n" <<
+	"4. Quit\n";
 }
 
 int init_service()
