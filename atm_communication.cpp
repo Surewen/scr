9ac355daa13a37e306edbd2eac21a50d35823bf7
Surewen <github.com>
2019-02-09 16:29:54 -0800

    added init and loop

--- atm_communication.cpp
+++ atm_communication.cpp
@@ -1,3 +1,22 @@
-int atm_communication(){
-	
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+
+bool communication_running = true;
+
+int init_communication();
+
+int atm_communication()
+{
+	init_communication();
+
+	while(communication_running)
+	{
+
+	}
+}
+
+int init_communication()
+{
+	std::cout << "atm_communication process created." << "\n";
 }
--- atm_service.cpp
+++ atm_service.cpp
@@ -1,6 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+
+bool service_running = true;
+
+int withdraw();
+int deposit();
+int draw_menu();
+int init_service();
+
 int atm_service()
 {
-	
+	init_service();
+
+	while(service_running)
+	{
+
+	}
 }
 
 int withdraw()
@@ -11,4 +27,14 @@ int withdraw()
 int deposit()
 {
 
+}
+
+int draw_menu()
+{
+	system("clear");
+}
+
+int init_service()
+{
+	std::cout << "atm_service process created." << "\n";
 }
