335684c75b9dea4cd800da8512c270c08d8e2ddb
Surewen <github.com>
2019-02-17 09:19:53 -0800

    pin verification

--- atm_service.cpp
+++ atm_service.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <ctime>
 
+using namespace std;
 bool service_running = true;
 
 
@@ -11,6 +12,7 @@ int withdraw();
 int deposit();
 int draw_menu();
 int init_service();
+bool pin_check(int pin);
 
 int atm_service()
 {
@@ -26,23 +28,29 @@ int atm_service()
 		{
 			previous_time = time(0);
 			//ALL LOGIC GOES HERE
-			draw_menu();
-			char key_pressed;
-			cin >> key_pressed;
-			switch(key_pressed)
+
+
+			if(pin_check(1234))
 			{
-				case 1: // show balance
-				break;
-				case 2: // withdraw cash
-				break;
-				case 3: // deposit cash
-				break;
-				case 4: // quit
-				exit(0);
-				break;
-				default:
-				break;
+				draw_menu();
+				char key_pressed;
+				cin >> key_pressed;
+				switch(key_pressed)
+				{
+					case 1: // show balance
+					break;
+					case 2: // withdraw cash
+					break;
+					case 3: // deposit cash
+					break;
+					case 4: // quit
+					exit(0);
+					break;
+					default:
+					break;
+				}
 			}
+			
 		}
 		current_time = time(0);
 		delta_time = current_time - previous_time;
@@ -73,4 +81,19 @@ int draw_menu()
 int init_service()
 {
 	std::cout << "atm_service process created." << "\n";
+}
+
+bool pin_check(int pin)
+{
+	int user_input_pin;                                            
+    cout << "Please Enter Your 4 Digit PIN\n> ";
+    cin >> user_input_pin;
+    if(user_input_pin == pin)
+    {
+    	return true;
+    }
+    else
+    {
+    	return false;
+    }
 }
