65c38d93ec2840db334cb63c5ac7ea487f30a368
Inimisaru <bartekbalak@gmail.com>
2019-02-22 13:20:39 +0100

    Finalna wersja

--- atm.cpp
+++ atm.cpp
@@ -1,29 +1,69 @@
+
+#include <cstdlib>
+#include <string>
 #include <iostream>
-#include <stdio.h>
-#include <unistd.h>
+#include <limits>
+#include <ccrtp/rtp.h>
+#include "atm_service.h"
+#include "atm_communication.h"
 
-#include <atm_communication.h>
-#include <atm_service.h>
+#ifdef  CCXX_NAMESPACES
+using namespace ost;
+using namespace std;
+#endif
 
-pid_t communications_pid = fork();
 
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
+	
+	int pipeStatus;
+	int pipeOutput[2]; // file descriptor
+
+	pipeStatus = pipe(pipeOutput); // create output read/write pipe ends
+	if (pipeStatus < 0) {
+	std::cerr << "ERROR CREATING PIPE" << std::endl;
+	exit(1);
+	}
+	
+	pid_t communications_pid = fork();
+	
 	if(communications_pid == 0)
 	{
 		//Communication process
-		atm_communication();
-		return 0;
+		Thread::sleep(100);
+		atm_communication *communicator = new atm_communication;
+		communicator->start();
+
 	}
 	else if (communications_pid > 0)
 	{
 		//Service process
-		atm_service();
-		return 0;
+		atm_service *atm_thread = new atm_service;
+		atm_thread->start();
+
 	}
 	else
 	{
 		std::cout << "Fork fail!" << '\n';
 	}
+	while(1);
 	return 0;
+	
+	
+	
+	/*
+	
+	char data[]="Witam";
+	char host[]="127.0.0.1";
+	char port[]="10000";
+	sendToBank(data,host,port);
+	
+	while(true);
+	
+    return 0;
+    */
 }
+
+
+
+
