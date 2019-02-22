7369344eaf82b0590d5e62438c467f8aa571557a
Inimisaru <bartekbalak@gmail.com>
2019-02-22 13:15:02 +0100

    Finalna wersja

--- atm_communication.h
+++ atm_communication.h
@@ -1,4 +1,101 @@
-#ifndef _atm_communication
-#define _atm_communication
-int atm_communication();
+#include <cstdlib>
+#include <string>
+#include <iostream>
+#include <limits>
+#include <ccrtp/rtp.h>
+#include "atm_communication_sender.h"
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sstream>
+
+#ifdef  CCXX_NAMESPACES
+using namespace ost;
+using namespace std;
 #endif
+
+const int RECEIVER_BASE = 11000; //port do nasluchiwania
+class atm_communication: public Thread
+{
+private:
+	RTPSession *socket;
+	InetHostAddress local_ip;
+	uint32 ssrc;
+	
+public:
+	atm_communication(){
+		local_ip = "127.0.0.1";
+		if( ! local_ip ){  
+			cerr << "Rx: IP address is not correct!" << endl;
+			exit();
+		}
+		socket = new RTPSession(local_ip,RECEIVER_BASE);
+		ssrc = socket->getLocalSSRC();
+	}
+	~atm_communication(){
+		terminate();
+		delete socket;
+	}
+	int fd[2];
+	
+	void run(void){    
+		socket->setExpireTimeout(3000000);
+		socket->setPayloadFormat(StaticPayloadFormat(sptMP2T));
+		socket->startRunning();
+		int fd; 
+		char const * myfifo = "./myfifo"; 
+		char buf[50];
+		mkfifo(myfifo, 0666); 
+		
+		int fd2; 
+		char const * myfifo2 = "./myfifo2"; 
+		char buf2[50];
+		mkfifo(myfifo2, 0666); 
+
+
+		char host[]="127.0.0.1";
+		char port[]="10000";
+			
+		for( int i = 0 ; true ; i++ ){
+
+			fd = open(myfifo,O_RDONLY); 
+			read(fd, buf, 50); 
+			close(fd); 
+
+			sendToBank(buf,host,port);
+			
+			const AppDataUnit *adu = NULL;
+			while ( NULL == adu ) {
+				Thread::sleep(10);
+				adu = socket->getData(socket->getFirstTimestamp());
+			}		
+			time_t receiving_time = time(NULL);
+			char tmstring[30];
+			strftime(tmstring,30,"%X",localtime(&receiving_time));
+			     //OTRZYMANE DANE OD BANKU W ADU->GetData
+			    
+			    stringstream buffer;
+				buffer<< hex << adu->getData()<<endl;
+				string temp = buffer.str();
+				strcpy(buf2, temp.c_str());  
+			     
+			    fd2 = open(myfifo2, O_WRONLY); 
+				write(fd2, buf2, strlen(buf2)+1); 
+				close(fd2); 
+				
+
+			     //--------------------------------------
+			delete adu;
+			
+		}
+	}
+	void sendToBank(char data[],char host[],char port[]){
+	
+	char timestamp[]="1";
+	char count[]="1";
+	
+    Sender sender((unsigned char *)data, InetHostAddress(host),
+        atoi(port), atoi(timestamp), atoi(count));
+	}
+};
--- bank.cpp
+++ bank.cpp
@@ -1,132 +1,469 @@
-#include <iostream>
-#include <fstream>
-#include <string>
+
+#include <cstdlib>
 #include <sstream>
+#include <ccrtp/rtp.h>
+
+#ifdef  CCXX_NAMESPACES
+using namespace ost;
 using namespace std;
+#endif
+class Sender: public RTPSession, public TimerPort
+{
+public:
+    Sender(const unsigned char* data, const InetHostAddress& ia,
+    tpport_t port, uint32 tstamp, uint16 count) :
+    RTPSession(InetHostAddress("0.0.0.0")), packetsPerSecond(10)
+    {
+        uint32 timestamp = tstamp? tstamp : 0;
+        defaultApplication().setSDESItem(SDESItemTypeTOOL, "rtpsend demo app.");
+        setSchedulingTimeout(10000);
+        setExpireTimeout(1000000);
 
-ifstream accounts;
+        if ( !addDestination(ia,port) ) {
+            cerr << "Could not connect" << endl;
+            exit();
+        }
 
-string card_number = "";
-string pin;
-string balance;
-bool account_exists = false;
+        setPayloadFormat(StaticPayloadFormat(sptPCMU));
+        startRunning();
 
-string get_pin(string account_number);
-string get_balance(string account_number);
-void substract_balance(string account_number, int amount);
-void add_balance(string account_number, int amount);
+        uint16 tstampInc = getCurrentRTPClockRate()/packetsPerSecond;
+        uint32 period = 1000/packetsPerSecond;
+        TimerPort::setTimer(period);
+        for ( int i = 0; i < count ; i++ ) {
+            putData(timestamp + i*tstampInc,
+                data,strlen((char *)data) + 1);
+            Thread::sleep(TimerPort::getTimer());
+            TimerPort::incTimer(period);
+        }
+    }
 
-int main(int argc, char const *argv[])
-{	
+private:
+    const uint16 packetsPerSecond;
+};
+
+void send(char message[]){
+	
+	char host[]="127.0.0.1";
+	char port[]="11000";
+	char timestamp[]="10";
+	char count[]="1";
+	
+    Sender sender((unsigned char *)message, InetHostAddress(host),
+        atoi(port), atoi(timestamp), atoi(count));
+}
+
+class Listener: RTPSession
+{
+public:
+
+	ifstream accounts;
+	ifstream log;
+	
+	string card_number = "";
+	string pin;
+	string balance;
+	bool account_exists = false;
+
+    Listener(InetMcastAddress& ima, tpport_t port) :
+    RTPSession(ima,port) {}
+
+    Listener(InetHostAddress& ia, tpport_t port) :
+    RTPSession(ia,port) {}
+
+	void add_log(string log_line)
+	{
+	log.open("log.txt");
+	string line;
+	string full_file;
+	while(log.good())
+		{
+			getline(log,line,'\n');
+			full_file.append(line);
+			full_file.append("\n");
+		}
+	full_file.append(log_line);
+	
+	log.close();
+	ofstream out_file("log.txt");
+	out_file << full_file;
+	
+	}
+	string get_pin(string account_number)
+	{
+		accounts.open("accounts.csv");
+		while(accounts.good())
+		{
+			getline(accounts, card_number, ',');
+			getline(accounts, pin, ',');
+			getline(accounts, balance, '\n');
+			if(account_number == card_number)
+			{
+				account_exists = true;
+				break;
+			}
+		}
+		accounts.close();
+		if(account_exists)
+		{
+			return pin;
+		}
+		else
+		{
+			return "0";
+		}
+	}
+
+	string get_balance(string account_number)
+	{
+		accounts.open("accounts.csv");
+		while(accounts.good())
+		{
+			getline(accounts, card_number, ',');
+			getline(accounts, pin, ',');
+			getline(accounts, balance, '\n');
+			if(account_number == card_number)
+			{
+				account_exists = true;
+				break;
+			}
+		}
+		accounts.close();
+		if(account_exists)
+		{
+			return balance;
+		}
+		else
+		{
+			return "0";
+		}
+	}
+
+	bool substract_balance(string account_number, int amount)
+	{
+		bool flag=false;
+		string full_file;
+		accounts.open("accounts.csv");
+		while(accounts.good())
+		{
+			getline(accounts, card_number, ',');
+			if(card_number == "")
+				break;
+			full_file.append(card_number);
+			full_file.append(",");
+			getline(accounts, pin, ',');
+			full_file.append(pin);
+			full_file.append(",");
+			getline(accounts, balance, '\n');
+			if(card_number == account_number)
+			{
+				if(stoi(balance)<amount){
+					flag=false;
+				}else{
+				balance = to_string(stoi(balance) - amount);
+					flag=true;
+				}
+			}
+			full_file.append(balance);
+			full_file.append("\n");
+		}
+		accounts.close();
+		ofstream out_file("accounts.csv");
+		out_file << full_file;
+		return flag;
+	}
+
+	void add_balance(string account_number, int amount)
+	{
+		string full_file;
+		accounts.open("accounts.csv");
+		while(accounts.good())
+		{
+			getline(accounts, card_number, ',');
+			if(card_number == "")
+				break;
+			full_file.append(card_number);
+			full_file.append(",");
+			getline(accounts, pin, ',');
+			full_file.append(pin);
+			full_file.append(",");
+			getline(accounts, balance, '\n');
+			if(card_number == account_number)
+			{
+				balance = to_string(stoi(balance) + amount);
+			}
+			full_file.append(balance);
+			full_file.append("\n");
+		}
+		accounts.close();
+		ofstream out_file("accounts.csv");
+		out_file << full_file;
+	}
+
+	bool transfer(string sender, string receiver, int amount)
+	{
+		if(substract_balance(sender, amount)){
+		add_balance(receiver, amount);
+		return true;
+		}else{
+		return false;
+		}
+	}
+	
+    void listen()
+    {
+		char message[50];
+        defaultApplication().setSDESItem(SDESItemTypeTOOL,"rtplisten demo app.");
+        setExpireTimeout(1000000);
+        setPayloadFormat(StaticPayloadFormat(sptPCMU));
+        
+        startRunning();
+        
+        
+        for (;;) {
+            const AppDataUnit* adu;
+            while ( (adu = getData(getFirstTimestamp())) ) {
+				
+				//adu->getData - sprawdzenie co to za dane
+				
+				/*
+                cerr << "Request from: "
+                     << dec <<
+                    adu->getSource().getNetworkAddress()
+                     << ":"
+                     << adu->getSource().getDataTransportPort()<<endl;
+                     */
+                     
+                     stringstream loglinestream;
+                     
+                     loglinestream<< "Request from: "
+                     << dec <<
+					 adu->getSource().getNetworkAddress()
+                     << ":"
+                     << adu->getSource().getDataTransportPort()<<" ";
+                     string logline=loglinestream.str();
+
+
+                     stringstream buffer;
+				buffer<< hex << adu->getData()<<endl;
+				string temp = buffer.str();
+				//mozna podzielic tutaj stringa na skladowe i do tablic char powsadzac odpowiednie pola lub pozamieniac na liczby nawet
+				
+				size_t pos = 0;
+				string delimiter = ":";
+				string command_string = temp.substr(0, temp.find(delimiter));
+				command_string.substr(pos + delimiter.length());
+				temp.erase(0, temp.find(delimiter) + delimiter.length());
+				
+				string account_number;
+				string amount;
+				string account_number2;
+
+				char command_buf[1];  
+				strcpy(command_buf, command_string.c_str());  
+				//Po odebraniu danych nalezy je zdekodowac i wykonac odpowiednia akcje:
+				if(command_buf[0]=='1'){
+					
+					//numer konta
+					size_t pos = 0;
+					pos = temp.find(delimiter);
+					account_number = temp.substr(0, pos);
+					temp.erase(0, pos + delimiter.length());
+					
+					
+                     loglinestream<<"PIN request for account: "+account_number+". Sending PIN: "+get_pin(account_number)<<endl;
+                     logline=loglinestream.str();
+                      
+                     
+					//cout<<"PIN request for account: "+account_number+". Sending PIN: "+get_pin(account_number)<<endl;
+					
+					//Odeslanie PIN o danym numerze konta
+					string data = "2:";
+					data=data+get_pin(account_number)+string(":");
+					strcpy(message, data.c_str());
+					//getpin
+					
+					//-----------------------------------
+
+				}else 
+				if(command_buf[0]=='3'){
+					
+					//numer konta
+					size_t pos = 0;
+					pos = temp.find(delimiter);
+					account_number = temp.substr(0, pos);
+					
+					loglinestream<<"Request balance for account: "+account_number+" Sending current balance: "+get_balance(account_number)<<endl;
+                     logline=loglinestream.str();
+                    
+                     
+					temp.erase(0, pos + delimiter.length());
+					//Odeslanie salda o danym numerze konta
+					string data = "4:";
+					data=data+account_number+string(":")+get_balance(account_number)+string(":");
+					strcpy(message, data.c_str());
+					//-----------------------------------
+					
+				}else 
+				if(command_buf[0]=='5'){
+					
+					//numer konta
+					size_t pos = 0;
+					pos = temp.find(delimiter);
+					account_number = temp.substr(0, pos);
+					temp.erase(0, pos + delimiter.length());
+					
+					//kwota
+					pos = temp.find(delimiter);
+					amount = temp.substr(0, pos);
+					temp.erase(0, pos + delimiter.length());
+					
+					loglinestream<<"Withdrawal request for account: "+account_number+" Amount: "+amount<<endl;
+                     logline=loglinestream.str();
+                   
+                    
+					//Odeslanie potwierdzenia operacji
+					if(substract_balance(account_number,stoi(amount))){
+					loglinestream<<"Operation success. Sending approval."<<endl;
+                    logline=loglinestream.str();
+                    cout<<logline<<endl;
+                    
+		
+					string data = "7:";
+					strcpy(message, data.c_str());
+					}else{
+				
+					loglinestream<<"Operation failed. Not enough funds."<<endl;
+                    logline=loglinestream.str();
+                    cout<<logline<<endl;
+                    
+					string data = "8:";
+					strcpy(message, data.c_str());
+					}
+					
+					
+					//-----------------------------------
+					
+					
+				}else 
+				if(command_buf[0]=='6'){
+					
+					//numer konta
+					size_t pos = 0;
+					pos = temp.find(delimiter);
+					account_number = temp.substr(0, pos);
+					temp.erase(0, pos + delimiter.length());
+					
+					//kwota
+					pos = temp.find(delimiter);
+					amount = temp.substr(0, pos);
+					temp.erase(0, pos + delimiter.length());
+					
+					loglinestream<<"Deposit request for account: "+account_number+" Amount: "+amount<<endl;
+                    logline=loglinestream.str();
+                  
+					//Odeslanie potwierdzenia operacji
+					add_balance(account_number,stoi(amount));
+					
+					loglinestream<<"Operation success. Sending approval."<<endl;
+                    logline=loglinestream.str();
+  
+					string data = "7:";
+					strcpy(message, data.c_str());
+					//-----------------------------------
+					
+					
+				}else 
+				if(command_buf[0]=='9'){
+
+					//numer konta
+					size_t pos = 0;
+					pos = temp.find(delimiter);
+					account_number = temp.substr(0, pos);
+					temp.erase(0, pos + delimiter.length());
+
+					//numer konta2
+					pos = temp.find(delimiter);
+					account_number2 = temp.substr(0, pos);
+					temp.erase(0, pos + delimiter.length());
+
+					//kwota
+					pos = temp.find(delimiter);
+					amount = temp.substr(0, pos);
+					temp.erase(0, pos + delimiter.length());
+					
+					
+					loglinestream<<"Transfer request for account: "+account_number+" to: "+account_number2+" Amount: "+amount<<endl;
+                    logline=loglinestream.str();
+ 
+					
+					if(transfer(account_number,account_number2,stoi(amount))){
+						
+					loglinestream<<"Operation success. Sending approval."<<endl;
+                    logline=loglinestream.str();
+
+						string data = "7:";
+						strcpy(message, data.c_str());
+					}else{
+					
+					loglinestream<<"Operation failed. Not enough funds."<<endl;
+                    logline=loglinestream.str();
+
+						string data = "8:";
+						strcpy(message, data.c_str());
+					}
+					//Odeslanie potwierdzenia operacji
+					
+					//-----------------------------------
+					
+					
+				}
+				
+				 cout<<logline;
+                 add_log(logline);
+                 
+                 send(message);    
+
+                 
+                delete adu;
+            }
+            Thread::sleep(7);
+        }
+    }
+    
+    
+
+};
+void startListening(){
+	
+	InetMcastAddress ima;
+    try {
+        ima = InetMcastAddress("127.0.0.1");
+    } catch (...) { }
+
+    Listener *foo;
+    tpport_t port = atoi("10000");
+    if ( ima.isInetAddress() ) {
+        foo = new Listener(ima,port);
+    } else {
+        InetHostAddress ia("127.0.0.1");
+        foo = new Listener(ia,atoi("10000"));
+    }
+    foo->listen();
+    delete foo;
+	
+}
+int main(int argc, char *argv[]){
+    startListening();
+    
     return 0;
 }
 
-string get_pin(string account_number)
-{
-	accounts.open("accounts.csv");
-	while(accounts.good())
-	{
-		getline(accounts, card_number, ',');
-		getline(accounts, pin, ',');
-		getline(accounts, balance, '\n');
-		if(account_number == card_number)
-		{
-			account_exists = true;
-			break;
-		}
-	}
-	accounts.close();
-	if(account_exists)
-	{
-		return pin;
-	}
-	else
-	{
-		return "0";
-	}
-}
+	
 
-string get_balance(string account_number)
-{
-	accounts.open("accounts.csv");
-	while(accounts.good())
-	{
-		getline(accounts, card_number, ',');
-		getline(accounts, pin, ',');
-		getline(accounts, balance, '\n');
-		if(account_number == card_number)
-		{
-			account_exists = true;
-			break;
-		}
-	}
-	accounts.close();
-	if(account_exists)
-	{
-		return balance;
-	}
-	else
-	{
-		return "0";
-	}
-}
-
-void substract_balance(string account_number, int amount)
-{
-	string full_file;
-	accounts.open("accounts.csv");
-	while(accounts.good())
-	{
-		getline(accounts, card_number, ',');
-		if(card_number == "")
-			break;
-		full_file.append(card_number);
-		full_file.append(",");
-		getline(accounts, pin, ',');
-		full_file.append(pin);
-		full_file.append(",");
-		getline(accounts, balance, '\n');
-		if(card_number == account_number)
-		{
-			balance = to_string(stoi(balance) - amount);
-		}
-		full_file.append(balance);
-		full_file.append("\n");
-	}
-	accounts.close();
-	ofstream out_file("accounts.csv");
-	out_file << full_file;
-}
-
-void add_balance(string account_number, int amount)
-{
-	string full_file;
-	accounts.open("accounts.csv");
-	while(accounts.good())
-	{
-		getline(accounts, card_number, ',');
-		if(card_number == "")
-			break;
-		full_file.append(card_number);
-		full_file.append(",");
-		getline(accounts, pin, ',');
-		full_file.append(pin);
-		full_file.append(",");
-		getline(accounts, balance, '\n');
-		if(card_number == account_number)
-		{
-			balance = to_string(stoi(balance) + amount);
-		}
-		full_file.append(balance);
-		full_file.append("\n");
-	}
-	accounts.close();
-	ofstream out_file("accounts.csv");
-	out_file << full_file;
-}
-
-void transfer(string sender, string receiver, int amount)
-{
-	substract_balance(sender, amount);
-	add_balance(receiver, amount);
-}
+/** EMACS **
+ * Local variables:
+ * mode: c++
+ * c-basic-offset: 4
+ * End:
+ */
