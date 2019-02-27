8cca2ee4a83ed18887dc28591156cb7c728213bc
Surewen <m.saniuk@wp.pl>
2019-02-27 19:59:31 +0100

    poprawa formatowania

--- atm_communication_sender.h
+++ atm_communication_sender.h
@@ -3,36 +3,35 @@
 #include <iostream>
 #include <limits>
 #include <ccrtp/rtp.h>
-class Sender: public RTPSession, public TimerPort
+class Sender : public RTPSession, public TimerPort
 {
-public:
-    Sender(const unsigned char* data, const InetHostAddress& ia,
-    tpport_t port, uint32 tstamp, uint16 count):
-    RTPSession(InetHostAddress("0.0.0.0")), packetsPerSecond(10)
+  public:
+    Sender(const unsigned char *data, const InetHostAddress &ia,
+           tpport_t port, uint32 tstamp, uint16 count) : RTPSession(InetHostAddress("0.0.0.0")), packetsPerSecond(10)
     {
-        uint32 timestamp = tstamp? tstamp : 0;
+        uint32 timestamp = tstamp ? tstamp : 0;
         defaultApplication().setSDESItem(SDESItemTypeTOOL, "rtpsend demo app.");
         setSchedulingTimeout(10000);
         setExpireTimeout(1000000);
-        if ( !addDestination(ia,port) ) 
+        if (!addDestination(ia, port))
         {
             cerr << "Could not connect" << endl;
             exit();
         }
         setPayloadFormat(StaticPayloadFormat(sptPCMU));
         startRunning();
-        uint16 tstampInc = getCurrentRTPClockRate()/packetsPerSecond;
-        uint32 period = 1000/packetsPerSecond;
+        uint16 tstampInc = getCurrentRTPClockRate() / packetsPerSecond;
+        uint32 period = 1000 / packetsPerSecond;
         TimerPort::setTimer(period);
-        for ( int i = 0; i < count ; i++ ) 
+        for (int i = 0; i < count; i++)
         {
-            putData(timestamp + i*tstampInc,
-                data,strlen((char *)data) + 1);
+            putData(timestamp + i * tstampInc,
+                    data, strlen((char *)data) + 1);
             Thread::sleep(TimerPort::getTimer());
             TimerPort::incTimer(period);
         }
     }
 
-private:
+  private:
     const uint16 packetsPerSecond;
 };
