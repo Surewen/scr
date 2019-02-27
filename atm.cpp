0721eb1a2531cca7fa3d15f04552299eb92a7dc0
Surewen <m.saniuk@wp.pl>
2019-02-27 18:45:29 +0100

    Poprawa formatowania

--- atm.cpp
+++ atm.cpp
@@ -20,39 +20,39 @@ int main(int argc, char *argv[])
 	string my_ip="127.0.0.1";
 	
     if (argc > 2) { 	
-	my_ip=argv[1];
-	host=argv[2];
-	string port="10000";
+	    my_ip=argv[1];
+	    host=argv[2];
+	    string port="10000";
 
-	pid_t communications_pid = fork();
+	    pid_t communications_pid = fork();
 	
-	if(communications_pid == 0)
-	{
-		//Communication process
-		Thread::sleep(100);
-		atm_communication *communicator = new atm_communication(my_ip,host);
-		communicator->start();
+        if(communications_pid == 0)
+	    {
+            //Communication process
+            Thread::sleep(100);
+            atm_communication *communicator = new atm_communication(my_ip,host);
+            communicator->start();
 
-	}
-	else if (communications_pid > 0)
-	{
-		//Service process
-		atm_service *atm_thread = new atm_service;
-		atm_thread->start();
+	    }
+	    else if (communications_pid > 0)
+	    {
+            //Service process
+            atm_service *atm_thread = new atm_service;
+            atm_thread->start();
 
+	    }
+	    else
+	    {
+		    std::cout << "Fork fail!" << '\n';
+	    }
+	    while(1);
+	    return 0;
 	}
 	else
 	{
-		std::cout << "Fork fail!" << '\n';
+		cout<<"No argument! ./atm <ip-adress>"<<endl;
+		return 0;
 	}
-	while(1);
-	return 0;
-		
-		
-		}else{
-			cout<<"No argument! ./atm <ip-adress>"<<endl;
-			return 0;
-		}
 	
 }
 
