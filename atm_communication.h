d88479fcc658a268cfaf75b0156e278f46babf18
Surewen <m.saniuk@wp.pl>
2019-02-27 19:58:04 +0100

    Dodanie komentarzy

--- atm_communication.h
+++ atm_communication.h
@@ -10,33 +10,38 @@
 #include <unistd.h>
 #include <sstream>
 
-#ifdef  CCXX_NAMESPACES
+#ifdef CCXX_NAMESPACES
 using namespace ost;
 using namespace std;
 #endif
 
 const int RECEIVER_BASE = 11000; //port do nasluchiwania
-class atm_communication: public Thread
+/**
+* @brief Klasa komunikacji z bankiem
+*
+* Odpowiedzialna za wykonywanie polaczen z bankiem nakazanych przez atm_service
+*/
+class atm_communication : public Thread
 {
-private:
+  private:
 	RTPSession *socket;
 	InetHostAddress local_ip;
 	uint32 ssrc;
-	
-public:
+
+  public:
 	string host;
-	
-	atm_communication(string my_ip,string bank_adr)
+
+	atm_communication(string my_ip, string bank_adr)
 	{
 		local_ip = my_ip.c_str();
-		if( ! local_ip )
-		{  
+		if (!local_ip)
+		{
 			cerr << "Rx: IP address is not correct!" << endl;
 			exit();
 		}
-		socket = new RTPSession(local_ip,RECEIVER_BASE);
+		socket = new RTPSession(local_ip, RECEIVER_BASE);
 		ssrc = socket->getLocalSSRC();
-		host=bank_adr;
+		host = bank_adr;
 	}
 	~atm_communication()
 	{
@@ -44,75 +49,76 @@ public:
 		delete socket;
 	}
 	int fd[2];
-	
+
 	void run(void)
-	{    
+	{
 		//BANK ADRESS-------
-		
+
 		//------------------
-		
+
 		socket->setExpireTimeout(3000000);
 		socket->setPayloadFormat(StaticPayloadFormat(sptMP2T));
 		socket->startRunning();
-		
-		//Utworzenie dwoch PIPE'ow jeden do odczytu drugi do zapisu
-		int fd; 
-		char const * myfifo = "./myfifo"; 
-		char buf[50];
-		mkfifo(myfifo, 0666); 
-		int fd2; 
-		char const * myfifo2 = "./myfifo2"; 
-		char buf2[50];
-		mkfifo(myfifo2, 0666); 
 
-		
-		
-			
-		for( int i = 0 ; true ; i++ )
+		//Utworzenie dwoch PIPE'ow jeden do odczytu drugi do zapisu
+		int fd;
+		char const *myfifo = "./myfifo";
+		char buf[50];
+		mkfifo(myfifo, 0666);
+		int fd2;
+		char const *myfifo2 = "./myfifo2";
+		char buf2[50];
+		mkfifo(myfifo2, 0666);
+
+		for (int i = 0; true; i++)
 		{
 
 			//Odczytanie PIPE w celu pobrania danych do przeslania do banku
-			fd = open(myfifo,O_RDONLY); 
-			read(fd, buf, 50); 
-			close(fd); 
-			
+			fd = open(myfifo, O_RDONLY);
+			read(fd, buf, 50);
+			close(fd);
+
 			//Wyslanie zadania do banku
-			sendToBank(buf,host);
-			
+			sendToBank(buf, host);
+
 			const AppDataUnit *adu = NULL;
-			while ( NULL == adu ) 
+			while (NULL == adu)
 			{
 				Thread::sleep(10);
 				adu = socket->getData(socket->getFirstTimestamp());
-			}		
+			}
 			time_t receiving_time = time(NULL);
 			char tmstring[30];
-			strftime(tmstring,30,"%X",localtime(&receiving_time));
-				
-				//Przeksztalcenie otrzymanych danych do postaci tablicy char'ow
-			    stringstream buffer;
-				buffer<< hex << adu->getData()<<endl;
-				string temp = buffer.str();
-				strcpy(buf2, temp.c_str());  
-				
-			    //Przekazanie otrzymanych danych przy pomocy PIPE do procesu obslugi klienta
-			    fd2 = open(myfifo2, O_WRONLY); 
-				write(fd2, buf2, strlen(buf2)+1); 
-				close(fd2); 
+			strftime(tmstring, 30, "%X", localtime(&receiving_time));
+
+			//Przeksztalcenie otrzymanych danych do postaci tablicy char'ow
+			stringstream buffer;
+			buffer << hex << adu->getData() << endl;
+			string temp = buffer.str();
+			strcpy(buf2, temp.c_str());
+
+			//Przekazanie otrzymanych danych przy pomocy PIPE do procesu obslugi klienta
+			fd2 = open(myfifo2, O_WRONLY);
+			write(fd2, buf2, strlen(buf2) + 1);
+			close(fd2);
 
 			delete adu;
-			
 		}
 	}
-	
-	void sendToBank(char message[],string adress)
-	{//Wyslanie danych pod wskazany adres
-			
-	char port[]="10000";//Port na ktorym pracuje bank
-	char timestamp[]="1";
-	char count[]="1";
-	//Utworzenie obiektu odpowiedzialnego za utworzenie sesji RTP i wyslanie danych
-	Sender sender((unsigned char *)message, InetHostAddress(adress.c_str()),
-    atoi(port), atoi(timestamp), atoi(count)); 
+	/**
+	 * Metoda do przeslania danych do banku na podany adres IP.
+	 *
+	 * @param message Wiadomosc do przeslania w postaci tablicy znakow.
+	 * @param address Adres ip, do ktorego ma zostac wyslana wiadomosc.
+	 */
+	void sendToBank(char message[], string adress)
+	{
+
+		char port[] = "10000"; //Port na ktorym pracuje bank
+		char timestamp[] = "1";
+		char count[] = "1";
+		//Utworzenie obiektu odpowiedzialnego za utworzenie sesji RTP i wyslanie danych
+		Sender sender((unsigned char *)message, InetHostAddress(adress.c_str()),
+					  atoi(port), atoi(timestamp), atoi(count));
 	}
 };
